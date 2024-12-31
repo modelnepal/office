7419642415c9e2b510e10427c6713766829d67c1
modelnepal <94734572+amreshthakur@users.noreply.github.com>
2024-12-31 18:16:03 +0545

    1 footer themes

--- footer.php
+++ footer.php
@@ -12,19 +12,11 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( '/office', 'domain' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'domain' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'domain' ), 'Designed', '<a href="/office">Cyberpur company</a>' );
-				?>
-		</div><!-- .site-info -->
+		
+
+
+
+	
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
