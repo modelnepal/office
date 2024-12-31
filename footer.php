ca03ef4476418e0de97ae14005b9c4507a1c7280
modelnepal <94734572+amreshthakur@users.noreply.github.com>
2024-12-31 10:17:44 +0545

    footer section update

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'domain' ) ); ?>">
+			<a href="<?php echo esc_url( __( '/office', 'domain' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'domain' ), 'WordPress' );
@@ -22,7 +22,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'domain' ), 'domain', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'domain' ), 'Designed', '<a href="/office">Cyberpur company</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
